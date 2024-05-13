524f17894334b4ccecb2465fc310656484df3266
Adam Czerniak <adam11392@gmail.com>
2024-05-13 19:54:56 +0200

    Add Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import classes from "./Footer.module.css";
+
+function Footer() {
+  return (
+    <footer className={classes.footer}>
+      Copyright &copy; 2024 Adam Czerniak
+    </footer>
+  );
+}
+
+export default Footer;
--- src/pages/RootLayout.js
+++ src/pages/RootLayout.js
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 function RootLayout() {
   return (
     <div className="app">
       <Header />
       <Outlet />
+      <Footer />
     </div>
   );
 }
