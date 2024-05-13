8cac18477330a5cf49c978a6dd9328e67a791840
Adam Czerniak <adam11392@gmail.com>
2024-05-13 02:14:03 +0200

    Add random movie section on HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,7 +5,12 @@ function HomePage() {
   return (
     <main>
       <section className={classes.random_movie}>
-        <h1>Movie of the day</h1>
+        <div className={classes.section_title}>
+          <h1>Movie of the day</h1>
+          <h2>Title</h2>
+          <p>2020 • Sci-Fi | Action • PG-18</p>
+          <p>NETFLIX Disney+</p>
+        </div>
       </section>
       <RankSection title="Popular in cinemas" rank="cinema" />
       <RankSection title="Best on Netflix" rank="netflix" />
