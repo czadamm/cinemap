0ced2d51c1dc682060aef8a9f28d889999439701
Adam Czerniak <adam11392@gmail.com>
2024-05-16 23:59:28 +0200

    Add credits to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,18 @@
 import classes from "./Footer.module.css";
+import logo from "../assets/tmdb-full.svg";
 
 function Footer() {
   return (
     <footer className={classes.footer}>
-      Copyright &copy; 2024 Adam Czerniak
+      <p className={classes.credits}>
+        <span>Powered by</span>
+        <span className={classes.tmdb_logo}>
+          <a href="https://www.themoviedb.org/">
+            <img src={logo} alt="TMDB logo" />
+          </a>
+        </span>
+      </p>
+      <p>Copyright &copy; 2024 Adam Czerniak</p>
     </footer>
   );
 }
