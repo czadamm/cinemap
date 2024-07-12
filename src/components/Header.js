b555f2a0b4a1d98b6530618329de80835e42572f
Adam Czerniak <adam11392@gmail.com>
2024-07-13 00:17:28 +0200

    Add framer-motion and scroll effect to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,49 +1,49 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import { useMotionValueEvent, useScroll } from "framer-motion";
 
 import classes from "./Header.module.css";
-
 import logoLight from "../assets/logo-light.svg";
-// import logoDark from "../assets/logo-dark.svg";
 
 function Header() {
-  const [openSettings, setOpenSettings] = useState(false);
+  const [isPageScrolled, setIsPageScrolled] = useState(false);
+  const { scrollY } = useScroll();
 
-  const handleToggleSettings = () => {
-    setOpenSettings((prevState) => !prevState);
-  };
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    if (latest > 20) {
+      setIsPageScrolled(true);
+    } else {
+      setIsPageScrolled(false);
+    }
+  });
 
   return (
-    <header className={classes.header}>
-      <img src={logoLight} alt="CINEMAP" />
-      <nav className={classes.nav_top}>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="library">Library</Link>
-          </li>
-          <li>
-            <Link to="upcoming">Upcoming</Link>
-          </li>
-          <li>
-            <Link to="sign-in" className={classes.button}>
-              Sign-in
-            </Link>
-          </li>
-          <li>
-            <i className="fa-solid fa-gear" onClick={handleToggleSettings}></i>
-            <div
-              id={classes.settings_window}
-              className={openSettings ? classes.active : undefined}
-            >
-              <p>Theme</p>
-              <p>Language</p>
-            </div>
-          </li>
-        </ul>
-      </nav>
+    <header
+      className={`${classes.header} ${isPageScrolled && classes.scrolled}`}
+    >
+      <div className={classes.nav_wrapper}>
+        <Link to="/">
+          <img src={logoLight} alt="CINEMAP" />
+        </Link>
+        <nav className={classes.nav_top}>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="library">Library</Link>
+            </li>
+            <li>
+              <Link to="upcoming">Upcoming</Link>
+            </li>
+            <li>
+              <Link to="sign-in" className={classes.button}>
+                Sign-in
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
