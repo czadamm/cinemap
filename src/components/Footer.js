48415c49491e80b120aab7f5bc316ca7b48dabe4
Adam Czerniak <adam11392@gmail.com>
2024-07-14 13:34:09 +0200

    Add footer navigation

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,28 @@
 import classes from "./Footer.module.css";
 import logo from "../assets/tmdb-full.svg";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
     <footer className={classes.footer}>
-      <p className={classes.credits}>
-        <span>Powered by</span>
-        <span className={classes.tmdb_logo}>
-          <a href="https://www.themoviedb.org/">
-            <img src={logo} alt="TMDB logo" />
-          </a>
-        </span>
-      </p>
-      <p>Copyright &copy; 2024 Adam Czerniak</p>
+      <div className={classes.copyrights}>
+        <p>2024 &copy; Adam Czerniak</p>
+        <p className={classes.credits}>
+          <span>Powered by</span>
+          <span className={classes.tmdb_logo}>
+            <a href="https://www.themoviedb.org/">
+              <img src={logo} alt="TMDB logo" />
+            </a>
+          </span>
+        </p>
+      </div>
+      <div className={classes.navigation}>
+        <Link to="/">Home</Link>
+        <Link to="/library">Library</Link>
+        <Link to="/upcoming">Upcoming</Link>
+        <Link to="/help">Help</Link>
+        <Link to="/cookies">Cookies</Link>
+      </div>
     </footer>
   );
 }
