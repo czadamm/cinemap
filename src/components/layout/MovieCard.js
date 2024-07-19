d9ed35ae5af9324aace79fe2ecc9112af290cf3d
Adam Czerniak <adam11392@gmail.com>
2024-07-19 17:37:14 +0200

    Add movie info after card hover

--- src/components/layout/MovieCard.js
+++ src/components/layout/MovieCard.js
@@ -1,6 +1,6 @@
 import classes from "./MovieCard.module.css";
 
-function Movie({ image, title, rate }) {
+function Movie({ image, title, year, rate }) {
   return (
     <li className={classes.movie}>
       <img src={image} alt={title} />
@@ -8,6 +8,10 @@ function Movie({ image, title, rate }) {
         <i className="fa-regular fa-heart"></i>
       </div>
       <div className={classes.rating}>{rate.toFixed(1)}</div>
+      <div className={classes.movie_info}>
+        <h3>{title}</h3>
+        <p>{year.slice(0, 4)}</p>
+      </div>
     </li>
   );
 }
