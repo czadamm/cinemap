5b778b7f74205780b5076d4d39d5b4a9fa25183d
Adam Czerniak <adam11392@gmail.com>
2024-07-24 00:12:33 +0200

    Change styles paths

--- src/layouts/RootLayout.js
+++ src/layouts/RootLayout.js
@@ -1,16 +1,17 @@
 import { Outlet } from "react-router";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import classes from "./RootLayout.module.css";
 
 function RootLayout() {
   return (
-    <div className="app">
+    <>
       <Header />
-      <main>
+      <main className={classes.main}>
         <Outlet />
       </main>
       <Footer />
-    </div>
+    </>
   );
 }
 
