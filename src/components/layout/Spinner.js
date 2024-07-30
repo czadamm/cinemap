8bc81322bb6e47a14c9f811ba9823b6dce25e68f
Adam Czerniak <adam11392@gmail.com>
2024-07-30 15:47:26 +0200

    Change spinner effect

--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -2,8 +2,14 @@ import classes from "./Spinner.module.css";
 
 function Spinner() {
   return (
-    <div className={classes.spinner_wrapper}>
-      <span className={classes.spinner}></span>
+    // <div className={classes.spinner_wrapper}>
+    //   <span className={classes.spinner}></span>
+    // </div>
+    <div className={classes.loader}>
+      <div className={classes.circle}>
+        <div className={classes.dot}></div>
+        <div className={classes.outline}></div>
+      </div>
     </div>
   );
 }
