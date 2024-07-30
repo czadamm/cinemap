d599b721fa4902b02e67845f351dd1c1b030fcbb
Adam Czerniak <adam11392@gmail.com>
2024-07-30 15:49:13 +0200

    Add ErrorPage background gradients

--- src/pages/errors/ErrorPage.js
+++ src/pages/errors/ErrorPage.js
@@ -4,15 +4,17 @@ import classes from "./ErrorPage.module.css";
 
 function ErrorPage() {
   return (
-    <div className="app">
-      <Header />
-      <main className={classes.main}>
-        <h2 className={classes.error_code}>404 Not found</h2>
-        <p className={classes.error_message}>
-          We couldn't load this page. Check an address or try again later{" "}
-        </p>
-      </main>
-      <Footer />
+    <div className={classes.background}>
+      <div className={classes.background_blur}>
+        <Header />
+        <main className={classes.main}>
+          <h2 className={classes.error_code}>404 Not found</h2>
+          <p className={classes.error_message}>
+            We couldn't load this page. Check an address or try again later{" "}
+          </p>
+        </main>
+        <Footer />
+      </div>
     </div>
   );
 }
