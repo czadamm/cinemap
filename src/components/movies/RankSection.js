f425588e8229c2541fa1005da68b0d9eb6d7b27f
Adam Czerniak <adam11392@gmail.com>
2024-08-03 22:08:53 +0200

    Add Title component

--- src/components/movies/RankSection.js
+++ src/components/movies/RankSection.js
@@ -1,10 +1,13 @@
+import Title from "../layout/Title";
 import Movies from "./Movies";
 import classes from "./RankSection.module.css";
 
 function RankSection(props) {
   return (
     <section className={classes.section}>
-      <h2>{props.title}</h2>
+      <div className={classes.section_title}>
+        <Title>{props.title}</Title>
+      </div>
       <div className={classes.rank}>
         <Movies
           categories={props.categories}
--- src/pages/UpcomingPage.js
+++ src/pages/UpcomingPage.js
@@ -1,10 +1,15 @@
+import Title from "../components/layout/Title";
 import Movies from "../components/movies/Movies";
+import classes from "./UpcomingPage.module.css";
 
 function UpcomingPage() {
   return (
-    <>
+    <div className={classes.content_wrapper}>
+      <div className={classes.section_title}>
+        <Title>Soon in theaters</Title>
+      </div>
       <Movies upcoming={true} />
-    </>
+    </div>
   );
 }
 
